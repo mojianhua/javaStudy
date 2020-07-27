3cc0a2221b417909834ffe7905a2bb430b52b7d8
饭桶吉 <1657210793@qq.com>
2020-07-27 16:58:04 +0800

    修改连接数据包

--- SpringStudy01/src/test/MysqlTest.java
+++ SpringStudy01/src/test/MysqlTest.java
@@ -11,9 +11,8 @@ public class MysqlTest {
     private static ConfigurableApplicationContext iocTest1 = new ClassPathXmlApplicationContext("applicationContent02.xml");
 
     public static void main(String[] args){
-        System.out.println("123");
-//        //1、容器中拿到连接池
-//        DataSource mysql01 = (DataSource) iocTest1.getBean("mysqlDb1");
-//        System.out.println(mysql01);
+        //1、容器中拿到连接池
+        DataSource mysql01 = (DataSource) iocTest1.getBean("dataSource");
+        System.out.println(mysql01);
     }
 }
