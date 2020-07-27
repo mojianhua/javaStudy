7597be7386dc9bcb56828f25fcf73efd847668fd
饭桶吉 <1657210793@qq.com>
2020-07-27 19:33:46 +0800

    bean引用外部文件

--- SpringStudy01/src/test/MysqlTest.java
+++ SpringStudy01/src/test/MysqlTest.java
@@ -1,18 +1,28 @@
 package test;
 
 
+import bean.CarNew;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.management.MBeanAttributeInfo;
 import javax.sql.DataSource;
 
 public class MysqlTest {
 
-    private static ConfigurableApplicationContext iocTest1 = new ClassPathXmlApplicationContext("applicationContent02.xml");
+    private static ConfigurableApplicationContext iocTest1 = new ClassPathXmlApplicationContext("conf/applicationContent02.xml");
 
     public static void main(String[] args){
-        //1、容器中拿到连接池
-        DataSource mysql01 = (DataSource) iocTest1.getBean("dataSource");
-        System.out.println(mysql01);
+//        //1、容器中拿到连接池
+//        DataSource mysql01 = (DataSource) iocTest1.getBean("dataSource");
+//        System.out.println(mysql01);
+
+//        //2、按照类型获取组件，可以获取到这个类型下的所有体现类子类等等
+//        DataSource mysql02 = iocTest1.getBean(DataSource.class);
+//        System.out.println(mysql02.getConnection());
+
+        //获取car值
+        CarNew car1 = iocTest1.getBean(CarNew.class);
+        System.out.println(car1);
     }
 }
