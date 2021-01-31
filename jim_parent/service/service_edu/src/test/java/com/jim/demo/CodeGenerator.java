25c2d862f05f4217b7960b377eabe3fd3ce595fa
mojianhua <1657210793@qq.com>
2021-01-31 17:13:21 +0800

    项目初始化

---
+++ jim_parent/service/service_edu/src/test/java/com/jim/demo/CodeGenerator.java
@@ -0,0 +1,66 @@
+package com.jim.demo;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.junit.Test;
+
+public class CodeGenerator {
+    @Test
+    public void run(){
+        //1、创建代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        //2、全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("testjava");
+        gc.setOpen(false);    //生成后是否打开资源管理器
+        gc.setFileOverride(false); //重新生成时文件是否覆盖
+        gc.setServiceName("%sService"); //去掉Service接口的首字母I
+        gc.setIdType(IdType.ID_WORKER); //主键策略
+        gc.setDateType(DateType.ONLY_DATE); //定于生成实体类日期类型
+        gc.setSwagger2(true); //是否开启Swagger2模式
+
+        //3、数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:8889/guli?useUnicode=true&characterEncoding=utf-8&serverTimezone=Asia/Shanghai&useSSL=false");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        dsc.setDbType(DbType.MYSQL);
+        mpg.setDataSource(dsc);
+
+        //4、包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName("edu"); //模块名
+        pc.setParent("com.example.demo");
+        pc.setController("controller");
+        pc.setEntity("entity");
+        pc.setMapper("mapper");
+        mpg.setPackageInfo(pc);
+
+        //策略配置
+        StrategyConfig strategy= new StrategyConfig();
+        strategy.setInclude("edu_teacher");          //设置生成表，如果多个表则逗号隔开，如：strategy.setInclude("edu_teacher","edu_teacher2");
+        strategy.setTablePrefix(pc.getModuleName() + "_");  //生成实体时去掉前缀，如：create_at 变成 createTime
+
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel); //数据库字段映射到实体的策略名称
+        strategy.setEntityLombokModel(true); //lomko模型 @Accessors(chain = true) setter操作
+
+        strategy.setRestControllerStyle(true); //restful api风格控制器
+        strategy.setControllerMappingHyphenStyle(true); //url中驼峰转字符串
+
+        mpg.setStrategy(strategy);
+
+        //执行
+        mpg.execute();
+    }
+}
